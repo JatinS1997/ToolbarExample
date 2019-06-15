e3d0f554e10505933f77f4257963cbba818024bb
Jatin Sharma <Jatin.sharma7983@gmail.com>
2019-06-15 18:02:03 +0530

    Toolbar

--- app/src/main/java/com/example/toolbarexample/MainActivity.java
+++ app/src/main/java/com/example/toolbarexample/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.toolbarexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
